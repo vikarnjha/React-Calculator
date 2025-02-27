49a18daab85329e8c14f4c386d3e59f89ea52474
Vikarn Jha <vikarnjha91@gmail.com>
2025-02-27 19:22:48 +0530

    added locgic for calculator

--- react-calculator/src/components/Calculator.jsx
+++ react-calculator/src/components/Calculator.jsx
@@ -1,41 +1,57 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDeleteLeft } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from 'react'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faDeleteLeft } from '@fortawesome/free-solid-svg-icons';
 
 const Calculator = () => {
+    const [data, setData] = useState("");
+
+    const getValue = (e) => {
+        console.log(e.target.value);
+        setData(data.concat(e.target.value));
+    }
+    const calculate = () => {
+        setData(eval(data).toString());
+    }
+    const clear = () => {
+        setData("");
+    }
+    const backSpace = () => {
+        setData(data.slice(0, -1));
+    }
+
   return (
     <>
     <div className="container">
         <span>Calculator</span>
         <div className="input">
-            <input placeholder='0' />
+            <input placeholder='0' value={data}/>
         </div>
-        <button value="(">(</button>
-        <button value=")">)</button>
-        <button value="%">%</button>
-        <button value="AC">AC</button>
+        <button onClick={getValue} value="(">(</button>
+        <button onClick={getValue} value=")">)</button>
+        <button onClick={getValue} value="%">%</button>
+        <button onClick={clear} value="AC">AC</button>
         
-        <button value="7">7</button>
-        <button value="8">8</button>
-        <button value="9">9</button>
-        <button value="/">/</button>
+        <button onClick={getValue} value="7">7</button>
+        <button onClick={getValue} value="8">8</button>
+        <button onClick={getValue} value="9">9</button>
+        <button onClick={getValue} value="/">/</button>
 
-        <button value="4">4</button>
-        <button value="5">5</button>
-        <button value="6">6</button>
-        <button value="*">x</button>
+        <button onClick={getValue} value="4">4</button>
+        <button onClick={getValue} value="5">5</button>
+        <button onClick={getValue} value="6">6</button>
+        <button onClick={getValue} value="*">x</button>
 
-        <button value="1">1</button>
-        <button value="2">2</button>
-        <button value="3">3</button>
-        <button value="+">+</button>
+        <button onClick={getValue} value="1">1</button>
+        <button onClick={getValue} value="2">2</button>
+        <button onClick={getValue} value="3">3</button>
+        <button onClick={getValue} value="+">+</button>
 
-        <button value="Back">
-        <FontAwesomeIcon icon={faDeleteLeft} />
+        <button onClick={backSpace} value="Back">Back
+        {/* <FontAwesomeIcon icon={faDeleteLeft} /> */}
         </button>
-        <button value="0">0</button>
-        <button value="=">=</button>
-        <button value="-">-</button>
+        <button onClick={getValue} value="0">0</button>
+        <button onClick={calculate} value="=">=</button>
+        <button onClick={getValue} value="-">-</button>
 
     </div>
 
